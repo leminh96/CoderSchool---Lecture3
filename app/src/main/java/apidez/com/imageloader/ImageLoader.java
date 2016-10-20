236771be3dad36726ee8ff203364e39518e3ffe2
Quan Vu <nongdenchet8435848@gmail.com>
2016-10-21 00:19:46 +0700

    done image loader

---
+++ app/src/main/java/apidez/com/imageloader/ImageLoader.java
@@ -0,0 +1,60 @@
+package apidez.com.imageloader;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by nongdenchet on 10/21/16.
+ */
+
+public class ImageLoader {
+
+    public void loadUrl(ImageView ivImage, String imagePath) {
+        new Loader(ivImage).execute(imagePath);
+    }
+
+    private class Loader extends AsyncTask<String, Integer, Bitmap> {
+        ImageView ivImage;
+
+        public Loader(ImageView ivImage) {
+            this.ivImage = ivImage;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap bitmap = null;
+            try {
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.d("Progress", String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null) {
+                ivImage.setImageBitmap(bitmap);
+            }
+        }
+    }
+}
--- app/src/main/java/apidez/com/imageloader/MainActivity.java
+++ app/src/main/java/apidez/com/imageloader/MainActivity.java
@@ -1,13 +1,18 @@
 package apidez.com.imageloader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivImage = (ImageView) findViewById(R.id.ivImage);
+        String url = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR7aE8c_B-sUW7sgcXJHsYUFx2Jp1FdJJQ9VroMEBgipouCW_fO";
+        new ImageLoader().loadUrl(ivImage, url);
     }
 }
